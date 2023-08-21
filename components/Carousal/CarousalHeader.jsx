fe5314e7fd06bacbf22104f35d9f8e479bb5f07c
Saad <saad.salman98@gmail.com>
2023-08-21 09:40:41 +0500

    initial commit

--- components/Carousal/CarousalHeader.jsx
+++ components/Carousal/CarousalHeader.jsx
@@ -16,16 +16,16 @@ const CarousalHeader = () => {
                 <div className="hidden md:flex space-x-4">
                     <div className="relative group">
                         <span className="cursor-pointer text-white mx-3">Vitamins & Supplements </span>
-                        <div className="absolute left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
+                        <div className="absolute z-30 left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
                             <a href="#" className="block text-gray-600">Sublink 1</a>
-                            <a href="#" className="block text-gray-600">Sublink 2</a>
+                            <a href="#" className="block text-gray-600 ">Sublink 2</a>
                         </div>
                     </div>
                 </div>
                 <div className="hidden md:flex space-x-4">
                     <div className="relative group">
                         <span className="cursor-pointer text-white mx-3">Devices & Appliances</span>
-                        <div className="absolute left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
+                        <div className="absolute z-30 left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
                             <a href="#" className="block text-gray-600">Sublink 1</a>
                             <a href="#" className="block text-gray-600">Sublink 2</a>
                         </div>
@@ -34,7 +34,7 @@ const CarousalHeader = () => {
                 <div className="hidden md:flex space-x-4">
                     <div className="relative group">
                         <span className="cursor-pointer text-white mx-3">Hygiene & Household</span>
-                        <div className="absolute left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
+                        <div className="absolute z-30 left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
                             <a href="#" className="block text-gray-600">Sublink 1</a>
                             <a href="#" className="block text-gray-600">Sublink 2</a>
                         </div>
@@ -43,7 +43,7 @@ const CarousalHeader = () => {
                 <div className="hidden md:flex space-x-4">
                     <div className="relative group">
                         <span className="cursor-pointer text-white mx-3">Health Conditions</span>
-                        <div className="absolute left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
+                        <div className="absolute z-30 left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
                             <a href="#" className="block text-gray-600">Sublink 1</a>
                             <a href="#" className="block text-gray-600">Sublink 2</a>
                         </div>
@@ -52,7 +52,7 @@ const CarousalHeader = () => {
                 <div className="hidden md:flex space-x-4">
                     <div className="relative group">
                         <span className="cursor-pointer text-white mx-3">Homeopathy</span>
-                        <div className="absolute left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
+                        <div className="absolute z-30 left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
                             <a href="#" className="block text-gray-600">Sublink 1</a>
                             <a href="#" className="block text-gray-600">Sublink 2</a>
                         </div>
@@ -60,7 +60,7 @@ const CarousalHeader = () => {
                 </div>
                 <div className="md:hidden">
                     <button
-                        className="text-white"
+                        className="text-white ml-10 w-[40px]"
                         onClick={toggleMenu}
                     >
                         ☰
@@ -71,35 +71,35 @@ const CarousalHeader = () => {
                 <div className="mt-2 space-y-2 bg-white w-full p-2 md:hidden">
                     <div className="relative group">
                         <span className="cursor-pointer text-gray-600">Vitamins & Supplements</span>
-                        <div className="absolute left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
+                        <div className="absolute z-30 left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
                             <a href="#" className="block text-gray-600">Sublink 1</a>
                             <a href="#" className="block text-gray-600">Sublink 2</a>
                         </div>
                     </div>
                     <div className="relative group">
                         <span className="cursor-pointer text-gray-600">Devices & Appliances</span>
-                        <div className="absolute left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
+                        <div className="absolute z-30 left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
                             <a href="#" className="block text-gray-600">Sublink 1</a>
                             <a href="#" className="block text-gray-600">Sublink 2</a>
                         </div>
                     </div>
                     <div className="relative group">
                         <span className="cursor-pointer text-gray-600">Hygiene & Household</span>
-                        <div className="absolute left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
+                        <div className="absolute z-30 left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
                             <a href="#" className="block text-gray-600">Sublink 1</a>
                             <a href="#" className="block text-gray-600">Sublink 2</a>
                         </div>
                     </div>
                     <div className="relative group">
                         <span className="cursor-pointer text-gray-600">Health Conditions</span>
-                        <div className="absolute left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
+                        <div className="absolute z-30 left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
                             <a href="#" className="block text-gray-600">Sublink 1</a>
                             <a href="#" className="block text-gray-600">Sublink 2</a>
                         </div>
                     </div>
                     <div className="relative group">
                         <span className="cursor-pointer text-gray-600">Homeopathy</span>
-                        <div className="absolute left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
+                        <div className="absolute z-30 left-0 hidden mt-2 space-y-2 bg-white w-36 p-2 group-hover:block">
                             <a href="#" className="block text-gray-600">Sublink 1</a>
                             <a href="#" className="block text-gray-600">Sublink 2</a>
                         </div>
